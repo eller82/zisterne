fa4c25e0635e9bad1e26c17756a722ab3c766785
eller82 <andre.mellentin@gmail.com>
2017-07-15 20:23:36 +0200

    adjusted device layouts + index homepage

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -3,6 +3,18 @@ class StatusController < ApplicationController
   before_action :authenticate_user!
 
   def index
+
+    @volumen = Volumen.order(messdatum: :desc).first
+
+    #Calculate percentage of luid level
+    if @volumen.volumen > 4500
+      @fluid = 0
+      @percentage = 100
+    else
+      @percentage = @volumen.volumen / 4500 * 100
+      @fluid = 100 - @percentage
+    end
+
   end
 
 
