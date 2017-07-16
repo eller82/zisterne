506c032de7c3f2510f107356dbe43dfebaeef800
eller82 <andre.mellentin@gmail.com>
2017-07-16 13:47:54 +0200

    added timezone configuration

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module Zisterne2
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
     config.i18n.default_locale = :de
+    config.time_zone = 'Berlin'
   end
 end
